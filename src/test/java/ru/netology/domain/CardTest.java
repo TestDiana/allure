3d524ef94bfc100cef42eec90bef27ec31fc21c5
Diana Vafina <Dianochka.25@mail.ru>
2021-07-21 14:33:57 +0300

    Changed build.gradle and test

--- src/test/java/ru/netology/domain/CardTest.java
+++ src/test/java/ru/netology/domain/CardTest.java
@@ -1,6 +1,10 @@
 package ru.netology.domain;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -12,6 +16,16 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CardTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
 
 
